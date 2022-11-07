0324132de80f89d79623d1c0afccacc01c3a1a12
Monahlissa <salissa4@gmail.com>
2022-11-07 16:51:57 -0600

    debug

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Post, Comment } = require('../models');
+const {
+    User,
+    Post,
+    Comment
+} = require('../models');
 
 
 router.get('/', (req, res) => {
