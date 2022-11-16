563e413c34431ec0fe5b2ff0eda4b0169938d531
Monahlissa <salissa4@gmail.com>
2022-11-15 18:12:42 -0600

    test9

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Build a CMS-style blog site developers will be able to publish their posts and c
 
 ## Usage
 
-Live on [Heroku](https://ancient-woodland-75045.herokuapp.com/) 
+Live on [Heroku](https://desolate-castle-99286.herokuapp.com/) 
 
 ![TechBlog](./public/images/demo.png)
 
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
 
-router.use('/Users', userRoutes);
-router.use('/Posts', postRoutes);
-router.use('/Comments', commentRoutes)
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
 
 module.exports = router;
